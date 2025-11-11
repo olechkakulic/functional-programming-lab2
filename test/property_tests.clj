283b7ed3ff8053bf2aad3bdc9780007c7a15f378
mac <336799@niuitmo.ru>
2025-11-11 18:41:36 +0300

    fix names

--- test/property_tests.clj
+++ test/property_tests.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]
-            [sc-set :as hm]))
+            [sc-set :as set]))
 
 (declare prop-add-then-find
          prop-remove-behaviour
@@ -14,8 +14,8 @@
   (gen/vector (gen/tuple gen/small-integer gen/small-integer)))
 
 (defn build [pairs]
-  (reduce (fn [acc [k v]] (hm/add k v acc))
-          (hm/empty)
+  (reduce (fn [acc [k v]] (set/add k v acc))
+          (set/empty)
           pairs))
 
 (defspec prop-add-then-find 200
@@ -23,29 +23,29 @@
                  k gen/small-integer
                  v gen/small-integer]
                 (let [m  (build pairs)
-                      m2 (hm/add k v m)]
-                  (= v (hm/try-find k m2)))))
+                      m2 (set/add k v m)]
+                  (= v (set/try-find k m2)))))
 
 (defspec prop-remove-behaviour 200
   (prop/for-all [pairs pair-gen
                  k gen/small-integer
                  v gen/small-integer]
                 (let [m        (build pairs)
-                      m-added  (hm/add k v m)
-                      m-removed (hm/remove k m-added)]
-                  (and (nil? (hm/try-find k m-removed))
+                      m-added  (set/add k v m)
+                      m-removed (set/remove k m-added)]
+                  (and (nil? (set/try-find k m-removed))
                        (every? (fn [[kk _]]
                                  (if (= kk k)
                                    true
-                                   (= (hm/try-find kk m-removed) (hm/try-find kk m))))
+                                   (= (set/try-find kk m-removed) (set/try-find kk m))))
                                pairs)))))
 
 (defspec prop-monoid-identity 200
   (prop/for-all [pairs pair-gen]
                 (let [m (build pairs)
-                      e (hm/empty)]
-                  (and (hm/equals m (hm/combine e m))
-                       (hm/equals m (hm/combine m e))))))
+                      e (set/empty)]
+                  (and (set/equals m (set/combine e m))
+                       (set/equals m (set/combine m e))))))
 
 (defspec prop-monoid-associative 200
   (prop/for-all [pa pair-gen
@@ -54,9 +54,9 @@
                 (let [a (build pa)
                       b (build pb)
                       c (build pc)
-                      left  (hm/combine a (hm/combine b c))
-                      right (hm/combine (hm/combine a b) c)]
-                  (hm/equals left right))))
+                      left  (set/combine a (set/combine b c))
+                      right (set/combine (set/combine a b) c)]
+                  (set/equals left right))))
 
 (def funcs (gen/elements [inc dec (fn [x] (* 2 x)) identity]))
 
@@ -65,6 +65,6 @@
                  f funcs
                  g funcs]
                 (let [m     (build pairs)
-                      left  (hm/map-values f (hm/map-values g m))
-                      right (hm/map-values (comp f g) m)]
-                  (hm/equals left right))))
+                      left  (set/map-values f (set/map-values g m))
+                      right (set/map-values (comp f g) m)]
+                  (set/equals left right))))
--- test/unit.clj
+++ test/unit.clj
@@ -1,66 +1,66 @@
 (ns unit
   (:require [clojure.test :refer [deftest is]]
-            [sc-set :as hm]
+            [sc-set :as set]
             [clojure.string :as str]))
 
 (defn build [pairs]
-  (reduce (fn [acc [k v]] (hm/add k v acc))
-          (hm/empty)
+  (reduce (fn [acc [k v]] (set/add k v acc))
+          (set/empty)
           pairs))
 
 (deftest add-and-try-find
   (let [m (build [[1 "one"] [2 "two"]])]
-    (is (= "one" (hm/try-find 1 m)))
-    (is (= "two" (hm/try-find 2 m)))))
+    (is (= "one" (set/try-find 1 m)))
+    (is (= "two" (set/try-find 2 m)))))
 
 (deftest remove-should-delete-key
   (let [m  (build [[1 "a"] [2 "b"]])
-        m2 (hm/remove 1 m)]
-    (is (nil? (hm/try-find 1 m2)))
-    (is (= "b" (hm/try-find 2 m2)))))
+        m2 (set/remove 1 m)]
+    (is (nil? (set/try-find 1 m2)))
+    (is (= "b" (set/try-find 2 m2)))))
 
 (deftest contains-key-detects-presence-and-absence
   (let [m (build [[1 "a"] [2 "b"]])]
-    (is (true? (hm/contains-key 1 m)))
-    (is (false? (hm/contains-key 3 m)))))
+    (is (true? (set/contains-key 1 m)))
+    (is (false? (set/contains-key 3 m)))))
 
 (deftest filter-removes-nonmatching
   (let [m (build [[1 "one"] [2 "two"]])
-        f (hm/filter (fn [k _] (zero? (mod k 2))) m)]
-    (is (nil? (hm/try-find 1 f)))
-    (is (= "two" (hm/try-find 2 f)))))
+        f (set/filter (fn [k _] (zero? (mod k 2))) m)]
+    (is (nil? (set/try-find 1 f)))
+    (is (= "two" (set/try-find 2 f)))))
 
 (deftest map-values-transforms-all-values
   (let [m (build [[1 "a"] [2 "b"]])
-        m2 (hm/map-values str/upper-case m)]
-    (is (= "A" (hm/try-find 1 m2)))
-    (is (= "B" (hm/try-find 2 m2)))))
+        m2 (set/map-values str/upper-case m)]
+    (is (= "A" (set/try-find 1 m2)))
+    (is (= "B" (set/try-find 2 m2)))))
 
 (deftest fold-accumulates-sum-of-keys
   (let [m (build [[1 "a"] [2 "b"] [3 "c"]])
-        result (hm/fold (fn [acc k _] (+ acc k)) 0 m)]
+        result (set/fold (fn [acc k _] (+ acc k)) 0 m)]
     (is (= 6 result))))
 
 (deftest combine-merges-and-overrides-duplicates
   (let [m1 (build [[1 "one"] [2 "two"]])
         m2 (build [[2 "TWO"] [3 "three"]])
-        result (hm/combine m2 m1)]
-    (is (= "one" (hm/try-find 1 result)))
-    (is (= "TWO" (hm/try-find 2 result)))
-    (is (= "three" (hm/try-find 3 result)))))
+        result (set/combine m2 m1)]
+    (is (= "one" (set/try-find 1 result)))
+    (is (= "TWO" (set/try-find 2 result)))
+    (is (= "three" (set/try-find 3 result)))))
 
 (deftest equals-compares-identical-maps
   (let [m1 (build [[2 "two"] [1 "one"]])
         m2 (build [[1 "one"] [2 "two"]])]
-    (is (true? (hm/equals m2 m1)))))
+    (is (true? (set/equals m2 m1)))))
 
 (deftest equals-detects-different-values
   (let [m1 (build [[1 "one"] [2 "two"]])
         m2 (build [[1 "one"] [2 "TWO"]])]
-    (is (false? (hm/equals m2 m1)))))
+    (is (false? (set/equals m2 m1)))))
 
 (deftest monoid-identity-with-empty
-  (let [empty-monoid (hm/empty)
+  (let [empty-monoid (set/empty)
         m (build [[1 "a"] [2 "b"]])]
-    (is (true? (hm/equals m (hm/combine empty-monoid m))))
-    (is (true? (hm/equals m (hm/combine m empty-monoid))))))
+    (is (true? (set/equals m (set/combine empty-monoid m))))
+    (is (true? (set/equals m (set/combine m empty-monoid))))))
